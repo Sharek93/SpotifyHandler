2a1a90ea1d431758764aa1030eb1208e0828ca5d
Kuba <kuba1493@gmail.com>
2019-03-17 23:32:26 +0100

    Add project files.

---
+++ SpotifyHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpotifyHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
